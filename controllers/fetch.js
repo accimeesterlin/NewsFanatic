0218b00298751d4f9420997a4bded365937987e5
AngelaF <ADFrys@users.noreply.github.com>
2018-04-11 15:28:30 -0500

    Modify controllers and front end view.

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -5,8 +5,6 @@ var Headline = require("../models/Headline.js");
 var index = require("../models/index.js");
 var axios = require("axios");
 
-// var path = require("path");
-
 
   module.exports = function(app) {
 
@@ -15,43 +13,10 @@ var axios = require("axios");
         if (error) {
           console.log(error);
         } else {
-          res.render("index");
+          res.render("index", {result: data});
         }
       })
   });
-
-// Route for retrieving all Notes from the db
-app.get("/notes", function(req, res) {
-  // Find all Notes
-  Note.find({})
-    .then(function(dbNote) {
-      // If all Notes are successfully found, send them back to the client
-      res.json(dbNote);
-    })
-    .catch(function(err) {
-      // If an error occurs, send the error back to the client
-      res.json(err);
-    });
-});
-
-// app.post("/submit", function(req, res) {
-//   // Create a new Note in the db
-//   db.Note.create(req.body)
-//     .then(function(Note) {
-//       // If a Note was created successfully, find one User (there's only one) and push the new Note's _id to the User's `notes` array
-//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-//       return db.Headline.findOneAndUpdate({}, { $push: { notes: Note._id } }, { new: true });
-//     })
-//     .then(function(dbUser) {
-//       // If the User was updated successfully, send it back to the client
-//       res.json(dbUser);
-//     })
-//     .catch(function(err) {
-//       // If an error occurs, send it back to the client
-//       res.json(err);
-//     });
-// });
 
 
 // Scrape data from one site and place it into the mongodb db
@@ -74,15 +39,16 @@ app.get("/scrape", function(req, res) {
         .attr("href");
 
       // Create a new Article using the `result` object built from scraping
-      headlines.create(result)
-        .then(function(dbArticle) {
+      Headline.create(result)
+        .then(function(dbHeadline) {
           // View the added result in the console
-          console.log(dbArticle);
+          console.log(dbHeadline);
         })
         .catch(function(err) {
           // If an error occurred, send it to the client
           return res.json(err);
         });
+        res.redirect("/")
     });
 
     // If we were able to successfully scrape and save an Article, send a message to the client
@@ -90,6 +56,8 @@ app.get("/scrape", function(req, res) {
     });
   });
 
+
 }
+
 
 
